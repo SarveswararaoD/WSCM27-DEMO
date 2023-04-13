47fd80538a84e683b568db9da8c5a9ecd4c8314c
SarveswararaoD <sarveswararao.333@gmail.com>
2023-04-13 11:50:02 +0530

    commit by Author

--- src/test/java/demo/Sample1.java
+++ src/test/java/demo/Sample1.java
@@ -4,9 +4,14 @@ import org.testng.annotations.Test;
 
 public class Sample1 {
 	@Test
-	public void sample()
+	public void deleteSample()
 	{
-		System.out.println("sample created");
+		//System.out.println("sample created");
+		
+		System.out.println("sample deleted");
+		
+		
+		System.out.println("Sample 2");
 	}
 
 }
